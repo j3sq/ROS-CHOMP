8bb0a2fecda66710d95be2821b59e468e17c22dc
J3SQ <jafar.qutteineh@gmail.com>
2015-07-26 23:29:12 +0200

    code beautify and fixes dor compiler warnings

--- chomp.hpp
+++ chomp.hpp
@@ -2,26 +2,26 @@
  *
  * Copyright (C) 2015 Jafar Qutteineh. All rights reserved.
  * License (3-Cluase BSD): https://github.com/j3sq/ROS-CHOMP/blob/master/LICENSE
-
-* This code uses and is based on code from:
-*   Project: trychomp https://github.com/poftwaresatent/trychomp
-*   Copyright (C) 2014 Roland Philippsen. All rights reserved.
-*   License (3-Clause BSD) : https://github.com/poftwaresatent/trychomp
-/**
-   \file chomp.hpp
-
-   CHOMP for point vehicles (x,y) moving holonomously in the plane. It will
-   plan a trajectory (xi) connecting start point (qs) to end point (qe) while
-   avoiding obstacles (obs)
-*/
+ *
+ * This code uses and is based on code from:
+ *   Project: trychomp https://github.com/poftwaresatent/trychomp
+ *   Copyright (C) 2014 Roland Philippsen. All rights reserved.
+ *   License (3-Clause BSD) : https://github.com/poftwaresatent/trychomp
+ * **
+ * \file chomp.hpp
+ *
+ * CHOMP for point vehicles (x,y) moving holonomously in the plane. It will
+ * plan a trajectory (xi) connecting start point (qs) to end point (qe) while
+ * avoiding obstacles (obs)
+ */
 
 #ifndef CHOMP_HPP
 #define CHOMP_HPP
 
-namespace chomp{
+namespace chomp {
 typedef Eigen::VectorXd Vector;
 typedef Eigen::MatrixXd Matrix;
 typedef Eigen::Isometry3d Transform;
-void chomp(Vector const &qs,Vector const &qe, Vector &xi,Matrix const &obs);
+void chomp(Vector const &qs, Vector const &qe, Vector &xi, Matrix const &obs);
 }
 #endif // CHOMP_HPP
